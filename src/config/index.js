63439dafe40583f61a5f8dca474dac46f825245d
Z-CatInDark <1152004209@qq.com>
2021-11-03 20:14:48 +0800

    修改url

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   baseUrl: {
     dev: 'http://localhost:3000',
-    pro: 'http://www.baidu.com'
+    pro: 'http://8.135.32.181:3001'
   },
   publicPath: [/\/public/, /\/login/]
 }
