5814f9cd198126874985e7359262ab833ec81f31
Z-CatInDark <1152004209@qq.com>
2021-11-04 17:32:23 +0800

    修改生产环境URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,8 @@
 export default {
   baseUrl: {
-    dev: 'http://localhost:3000',
-    pro: 'http//8.135.32.181/3000'
+    // dev: 'http://localhost:3000',
+    dev: '/api',
+    pro: 'http://8.135.32.181:3000'
   },
   publicPath: [/\/public/, /\/login/]
 }
